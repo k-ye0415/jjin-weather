627654ecf749986bed6b82bfa075c2248e60db56
YEJIN <ye_s_jin@naver.com>
2025-03-23 23:33:40 +0900

    splash -> loading -> onboarding -> temperature Screen 이동

---
+++ app/src/main/java/com/jin/jjinweather/layer/ui/onboarding/OnboardingScreen.kt
@@ -0,0 +1,39 @@
+package com.jin.jjinweather.layer.ui.onboarding
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.jin.jjinweather.layer.ui.Screens
+
+@Composable
+fun OnboardingScreen(navController: NavController) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding), contentAlignment = Alignment.Center
+        ) {
+            Column {
+                // todo : tutorial pager
+                //        permission
+                Text("Tutorial Screen", fontSize = 24.sp)
+                Button({
+                    // todo : event hoisting
+                    navController.navigate(Screens.TEMPERATURE.route) {
+                        popUpTo(Screens.ONBOARDING.route) { inclusive = true }
+                    }
+                }) { Text("GO TO MAIN") }
+            }
+        }
+    }
+}
