89c48d890932e415ad6375b57cf75ca475a1f376
YEJIN <ye_s_jin@naver.com>
2025-03-23 23:25:09 +0900

    splash 적용

--- app/src/main/java/com/jin/jjinweather/MainActivity.kt
+++ app/src/main/java/com/jin/jjinweather/MainActivity.kt
@@ -11,11 +11,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.jin.jjinweather.ui.theme.JJinWeatherTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+        var keepSplashScreen = true
+        splashScreen.setKeepOnScreenCondition { keepSplashScreen }
+        lifecycleScope.launch {
+            delay(1000L)
+            keepSplashScreen = false
+        }
         enableEdgeToEdge()
         setContent {
             JJinWeatherTheme {
