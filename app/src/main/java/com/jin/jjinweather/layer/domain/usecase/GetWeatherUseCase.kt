6c55f77136e81737c8e30c91d668eb3254038b84
YEJIN <ye_s_jin@naver.com>
2025-03-29 15:18:15 +0900

    expression body 로 변경.

--- app/src/main/java/com/jin/jjinweather/layer/domain/usecase/GetWeatherUseCase.kt
+++ app/src/main/java/com/jin/jjinweather/layer/domain/usecase/GetWeatherUseCase.kt
@@ -4,7 +4,5 @@ import com.jin.jjinweather.layer.domain.model.weather.Weather
 import com.jin.jjinweather.layer.domain.repository.WeatherRepository
 
 class GetWeatherUseCase(private val repository: WeatherRepository) {
-    suspend operator fun invoke(): Weather {
-        return repository.loadWeather()
-    }
+    suspend operator fun invoke(): Weather = repository.loadWeather()
 }
