57ee1b8e67fc76f79a6df6a88d89fe4c1a2a0cfe
YEJIN <ye_s_jin@naver.com>
2025-03-30 18:54:27 +0900

    weather API Error 를 throw 하지 않고 성공/실패로 나뉘어 처리

--- app/src/main/java/com/jin/jjinweather/layer/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/jin/jjinweather/layer/domain/repository/WeatherRepository.kt
@@ -3,5 +3,6 @@ package com.jin.jjinweather.layer.domain.repository
 import com.jin.jjinweather.layer.domain.model.weather.Weather
 
 interface WeatherRepository {
-    suspend fun loadWeather(): Weather
+    // 위치 정보 가져오는 부분에 따라 수정될 수 있음.
+    suspend fun loadWeather(): Result<Weather>
 }
