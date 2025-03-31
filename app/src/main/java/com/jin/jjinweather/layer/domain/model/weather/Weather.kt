2f4a5b229bdde17be4f062b8f6f64a506e116b83
YEJIN <ye_s_jin@naver.com>
2025-03-31 16:10:07 +0900

    Weather 관련 kdoc 추가 및 타입 수정

--- app/src/main/java/com/jin/jjinweather/layer/domain/model/weather/Weather.kt
+++ app/src/main/java/com/jin/jjinweather/layer/domain/model/weather/Weather.kt
@@ -1,5 +1,13 @@
 package com.jin.jjinweather.layer.domain.model.weather
 
+/**
+ * Weather
+ * property @moonPhase 설명
+ * 0.0 또는 1.0 -> New Moon(삭, 달이 태양과 일직선, 보이지 않음.)
+ * 0.25 -> First Quarter (상현달, 오늘쪽 반달)
+ * 0.5 -> Full Moon (보름달, 완전한 둥근달)
+ * 0.75 -> Last Quarter (하현달, 왼쪽 반달)
+ */
 data class Weather(
     val cityName: String,
     val iconResId: Int,
@@ -12,8 +20,4 @@ data class Weather(
     val sunrise: Long,
     val sunset: Long,
     val moonPhase: Double
-    //0.0 또는 1.0 -> New Moon(삭, 달이 태양과 일직선, 보이지 않음.)
-    //0.25 -> First Quarter (상현달, 오늘쪽 반달)
-    //0.5 -> Full Moon (보름달, 완전한 둥근달)
-    //0.75 -> Last Quarter (하현달, 왼쪽 반달)
 )
