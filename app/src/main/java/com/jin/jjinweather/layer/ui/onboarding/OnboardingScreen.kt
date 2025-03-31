0fae536e9c606066d1d21a0ba8e5325801d7a913
YEJIN <ye_s_jin@naver.com>
2025-03-31 16:57:40 +0900

    권환 요청 및 응답

--- app/src/main/java/com/jin/jjinweather/layer/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/jin/jjinweather/layer/ui/onboarding/OnboardingScreen.kt
@@ -1,5 +1,6 @@
 package com.jin.jjinweather.layer.ui.onboarding
 
+import android.Manifest
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,14 +9,30 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import com.jin.jjinweather.layer.domain.model.PermissionState
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun OnboardingScreen(viewModel: OnboardingViewModel, onNavigateToTemperature: () -> Unit) {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    val permissionState by viewModel.locationPermissionState.collectAsState()
+    val locationPermissionState = rememberPermissionState(
+        permission = Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
+    LaunchedEffect(locationPermissionState.status) {
+        viewModel.updateLocationPermissionStatus(locationPermissionState.status)
+    }
+
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -25,7 +42,22 @@ fun OnboardingScreen(viewModel: OnboardingViewModel, onNavigateToTemperature: ()
                 // todo : tutorial pager
                 //        permission
                 Text("Tutorial Screen", fontSize = 24.sp)
-                Button(onClick = onNavigateToTemperature) { Text("GO TO MAIN") }
+                when (permissionState) {
+                    PermissionState.GRANTED -> {
+                        Button(onClick = onNavigateToTemperature) {
+                            Text("GO TO MAIN")
+                        }
+                    }
+
+                    else -> {
+                        Text("날씨 정보를 위해 위치 권한이 필요해요.")
+                        Button(onClick = {
+                            locationPermissionState.launchPermissionRequest()
+                        }) {
+                            Text("권한 요청하기")
+                        }
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/jin/jjinweather/layer/ui/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/jin/jjinweather/layer/ui/onboarding/OnboardingViewModel.kt
@@ -1,6 +1,8 @@
 package com.jin.jjinweather.layer.ui.onboarding
 
 import androidx.lifecycle.ViewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
 import com.jin.jjinweather.layer.domain.model.PermissionState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -9,4 +11,15 @@ class OnboardingViewModel : ViewModel() {
     // todo : location permission check
     private val _locationPermissionState = MutableStateFlow(PermissionState.DENIED)
     val locationPermissionState: StateFlow<PermissionState> = _locationPermissionState
+
+    @OptIn(ExperimentalPermissionsApi::class)
+    fun updateLocationPermissionStatus(status: PermissionStatus) {
+        _locationPermissionState.value = when (status) {
+            is PermissionStatus.Granted -> PermissionState.GRANTED
+            is PermissionStatus.Denied -> {
+                if (status.shouldShowRationale) PermissionState.SHOW_RATIONALE
+                else PermissionState.DENIED
+            }
+        }
+    }
 }
