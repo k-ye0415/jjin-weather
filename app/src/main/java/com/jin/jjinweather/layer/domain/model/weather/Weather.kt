123462d06799e706ff13ee04e9cfc1084b93e51f
YEJIN <ye_s_jin@naver.com>
2025-04-01 20:02:07 +0900

    moonPhase 관련 kdoc 수정

--- app/src/main/java/com/jin/jjinweather/layer/domain/model/weather/Weather.kt
+++ app/src/main/java/com/jin/jjinweather/layer/domain/model/weather/Weather.kt
@@ -1,12 +1,11 @@
 package com.jin.jjinweather.layer.domain.model.weather
 
 /**
- * Weather
- * property @moonPhase 설명
- * 0.0 또는 1.0 -> New Moon(삭, 달이 태양과 일직선, 보이지 않음.)
- * 0.25 -> First Quarter (상현달, 오늘쪽 반달)
- * 0.5 -> Full Moon (보름달, 완전한 둥근달)
- * 0.75 -> Last Quarter (하현달, 왼쪽 반달)
+ * @property moonPhase 값의 범위는 0.0부터 1.0 까지 이며, 주요 값은 다음과 같다.
+ * - 0.0 또는 1.0 -> New Moon(삭, 달이 태양과 일직선, 보이지 않음.)
+ * - 0.25 -> First Quarter (상현달, 오늘쪽 반달)
+ * - 0.5 -> Full Moon (보름달, 완전한 둥근달)
+ * - 0.75 -> Last Quarter (하현달, 왼쪽 반달)
  */
 data class Weather(
     val cityName: String,
