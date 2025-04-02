a40c3c0ed4389d9b300e399a8af41c157709d99c
YEJIN <ye_s_jin@naver.com>
2025-04-02 18:34:32 +0900

    weather 프로퍼티 관련 kdoc 추가

--- app/src/main/java/com/jin/jjinweather/layer/domain/model/weather/Weather.kt
+++ app/src/main/java/com/jin/jjinweather/layer/domain/model/weather/Weather.kt
@@ -1,6 +1,9 @@
 package com.jin.jjinweather.layer.domain.model.weather
 
 /**
+ * @property yesterdayTemperature API 경로 분리
+ * @property minTemperature daily 에서 첫번째 인덱스 값으로 적용
+ * @property maxTemperature daily 에서 첫번째 인덱스 값으로 적용
  * @property iconResId String 으로 응답 받는다. d 와 n는 day 와 night.
  * - 01d or 01n -> clear sky
  * - 02d or 02n -> few clouds
@@ -12,6 +15,7 @@ package com.jin.jjinweather.layer.domain.model.weather
  * - 13d or 13n -> snow
  * - 50d or 50n -> mist
  * @property moonPhase 값의 범위는 0.0부터 1.0 까지 이며, 주요 값은 다음과 같다.
+ * - daily 에서 첫번째 인덱스 값으로 적용
  * - 0.0 또는 1.0 -> New Moon(삭, 달이 태양과 일직선, 보이지 않음.)
  * - 0.25 -> First Quarter (상현달, 오늘쪽 반달)
  * - 0.5 -> Full Moon (보름달, 완전한 둥근달)
