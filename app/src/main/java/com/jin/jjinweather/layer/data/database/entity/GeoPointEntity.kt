f1ca50284fc49c564cd0133f1ae98c545d9e9f60
YEJIN <ye_s_jin@naver.com>
2025-04-07 21:58:43 +0900

    TODO 작성
    - 동일한 위치의 정보가 새로운 데이터로 계속 저장됨으로, `locationIndex` 라는 key 값으로 update 처리 할 수 있도록 처리 예정.

--- app/src/main/java/com/jin/jjinweather/layer/data/database/entity/GeoPointEntity.kt
+++ app/src/main/java/com/jin/jjinweather/layer/data/database/entity/GeoPointEntity.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
  * 사용자의 위치 정보
  *
  * @property id 자동 증가되는 시퀀스 넘버
- * @property locationIndex
+ * @property locationIndex (TODO TemperatureScreen 정의 후 추가 될 예정)
  * - 기본값은 0 이며, 사용자의 현재 위치 정보를 나타냅니다.
  * - 이후 사용자가 위치를 지정하면, 지정된 위치마다 고유한 페이지 번호가 부여됩니다.
  * - 예: 현재 위치(서울): weatherPage = 0, 지정 위치(도쿄): weatherPage = 1 ...
