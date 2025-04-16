d8ff6d626d04a240f5da7e51a4b5dd200d8eada9
Jerry <jerry@f-lab.kr>
2025-04-16 22:37:14 +0900

    merge two repositories

--- app/src/main/java/com/jin/jjinweather/feature/location/GeoPointRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.jin.jjinweather.feature.location
-
-import com.jin.jjinweather.layer.domain.model.location.GeoPoint
-
-interface GeoPointRepository {
-    suspend fun currentGeoPoint(): GeoPoint
-}
--- app/src/main/java/com/jin/jjinweather/feature/location/LocationRepository.kt
+++ app/src/main/java/com/jin/jjinweather/feature/location/LocationRepository.kt
@@ -2,6 +2,8 @@ package com.jin.jjinweather.feature.location
 
 import com.jin.jjinweather.layer.domain.model.location.GeoPoint
 
-interface GeoCodeRepository {
+interface LocationRepository {
+    suspend fun currentGeoPoint(): GeoPoint
+
     suspend fun findCityNameAt(location: GeoPoint): String
 }
