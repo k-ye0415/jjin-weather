f35d3ce383435e282c244f2d961d7b31ce6ed437
YEJIN <ye_s_jin@naver.com>
2025-05-10 21:40:26 +0900

    Success Screen 추가

---
+++ app/src/main/java/com/jin/jjinweather/feature/temperature/ui/weathercontent/WeatherSuccessScreen.kt
@@ -0,0 +1,9 @@
+package com.jin.jjinweather.feature.temperature.ui.weathercontent
+
+import androidx.compose.runtime.Composable
+import com.jin.jjinweather.feature.weather.domain.model.CityWeather
+
+@Composable
+fun WeatherSuccessScreen(cityWeather: CityWeather) {
+
+}
