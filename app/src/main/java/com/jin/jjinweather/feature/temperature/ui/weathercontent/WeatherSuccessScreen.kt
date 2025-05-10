814828be40300de77b2304bfe9ce9a7436d727e2
YEJIN <ye_s_jin@naver.com>
2025-05-10 21:53:40 +0900

    SuccessScreen - top menu

--- app/src/main/java/com/jin/jjinweather/feature/temperature/ui/weathercontent/WeatherSuccessScreen.kt
+++ app/src/main/java/com/jin/jjinweather/feature/temperature/ui/weathercontent/WeatherSuccessScreen.kt
@@ -1,9 +1,40 @@
 package com.jin.jjinweather.feature.temperature.ui.weathercontent
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.jin.jjinweather.feature.temperature.ui.weathercontent.success.TopMenuAction
 import com.jin.jjinweather.feature.weather.domain.model.CityWeather
+import java.time.LocalTime
 
 @Composable
-fun WeatherSuccessScreen(cityWeather: CityWeather) {
+fun WeatherSuccessScreen(weather: CityWeather) {
+    val gradientBrush = Brush.verticalGradient(
+        colors = if (LocalTime.now().isAfter(weather.weather.dayWeather.sunrise)) {
+            listOf(Color(0xFF1d1c66), Color(0xFF7f6d8e))
+        } else {
+            listOf(Color(0xFF128cfe), Color(0xFF0074df))
+        }
+    )
 
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(brush = gradientBrush)
+                .padding(innerPadding)
+                .padding(horizontal = 18.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item { TopMenuAction() }
+        }
+    }
 }
---
+++ app/src/main/java/com/jin/jjinweather/feature/temperature/ui/weathercontent/success/TopMenuAction.kt
@@ -0,0 +1,38 @@
+package com.jin.jjinweather.feature.temperature.ui.weathercontent.success
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Dehaze
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun TopMenuAction() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        IconButton({}) {
+            Icon(
+                imageVector = Icons.Outlined.Dehaze,
+                contentDescription = "더보기",
+                tint = Color.White
+            )
+        }
+        Spacer(Modifier.weight(1f))
+        IconButton({}) {
+            Icon(
+                imageVector = Icons.Outlined.Add,
+                contentDescription = "추가",
+                tint = Color.White
+            )
+        }
+    }
+}
