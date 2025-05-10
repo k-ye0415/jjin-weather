c730fd88cb88c23bc90b35f49bbbc4c074a4c9b1
YEJIN <ye_s_jin@naver.com>
2025-05-10 22:25:39 +0900

    SuccessScreen - define yesterdayWeather and outfit section

--- app/src/main/java/com/jin/jjinweather/feature/temperature/ui/weathercontent/WeatherSuccessScreen.kt
+++ app/src/main/java/com/jin/jjinweather/feature/temperature/ui/weathercontent/WeatherSuccessScreen.kt
@@ -13,24 +13,27 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.jin.jjinweather.feature.temperature.ui.weathercontent.success.CurrentWeatherOverview
 import com.jin.jjinweather.feature.temperature.ui.weathercontent.success.TopMenuAction
+import com.jin.jjinweather.feature.temperature.ui.weathercontent.success.YesterdayWeatherOutfit
 import com.jin.jjinweather.feature.weather.domain.model.CityWeather
 import java.time.LocalTime
 
 @Composable
 fun WeatherSuccessScreen(weather: CityWeather) {
-    val gradientBrush = Brush.verticalGradient(
-        colors = if (LocalTime.now().isAfter(weather.weather.dayWeather.sunrise)) {
+    val isAfterSunset = LocalTime.now().isAfter(weather.weather.dayWeather.sunset)
+    val backgroundGradientBrush = Brush.verticalGradient(
+        colors = if (isAfterSunset) {
             listOf(Color(0xFF1d1c66), Color(0xFF7f6d8e))
         } else {
             listOf(Color(0xFF128cfe), Color(0xFF0074df))
         }
     )
+    val cardBackgroundColor = if (isAfterSunset) Color(0x501d1c66) else Color(0x50035198)
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .background(brush = gradientBrush)
+                .background(brush = backgroundGradientBrush)
                 .padding(innerPadding)
                 .padding(horizontal = 18.dp),
             horizontalAlignment = Alignment.CenterHorizontally
@@ -43,6 +46,12 @@ fun WeatherSuccessScreen(weather: CityWeather) {
                     currentWeatherIconRes = weather.weather.dayWeather.icon.drawableRes,
                     todayMinTemperature = weather.weather.dayWeather.temperatureRange.min.toInt(),
                     todayMaxTemperature = weather.weather.dayWeather.temperatureRange.max.toInt(),
+                    yesterdayTemperature = weather.weather.yesterdayWeather.temperature.toInt()
+                )
+            }
+            item {
+                YesterdayWeatherOutfit(
+                    backgroundColor = cardBackgroundColor,
                     yesterdayTemperature = weather.weather.yesterdayWeather.temperature.toInt()
                 )
             }
