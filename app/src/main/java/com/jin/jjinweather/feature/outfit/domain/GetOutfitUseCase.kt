e0846a47b201e0157ddd49bc3756f341505afb59
YEJIN <ye_s_jin@naver.com>
2025-05-14 23:49:37 +0900

    usecase 작성

---
+++ app/src/main/java/com/jin/jjinweather/feature/outfit/domain/GetOutfitUseCase.kt
@@ -0,0 +1,7 @@
+package com.jin.jjinweather.feature.outfit.domain
+
+class GetOutfitUseCase(private val outfitRepository: OutfitRepository) {
+    suspend operator fun invoke(temperature: Int): Result<String> {
+        return outfitRepository.generateOutfitImage(temperature)
+    }
+}
