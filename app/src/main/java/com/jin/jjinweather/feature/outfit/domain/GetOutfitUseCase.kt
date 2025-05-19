659f0f660955565e66c85e2f2d65bd1bccb832d9
YEJIN <ye_s_jin@naver.com>
2025-05-19 15:27:01 +0900

    outfit image 제작 요청하는 함수 명 수정 및 kdoc 작성

--- app/src/main/java/com/jin/jjinweather/feature/outfit/domain/GetOutfitUseCase.kt
+++ app/src/main/java/com/jin/jjinweather/feature/outfit/domain/GetOutfitUseCase.kt
@@ -2,6 +2,6 @@ package com.jin.jjinweather.feature.outfit.domain
 
 class GetOutfitUseCase(private val outfitRepository: OutfitRepository) {
     suspend operator fun invoke(temperature: Int): Result<String> {
-        return outfitRepository.generateOutfitImage(temperature)
+        return outfitRepository.generateOutfitImageUrl(temperature)
     }
 }
