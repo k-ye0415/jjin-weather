38d52a8cfb7722fa2680a2a93cbcb9e996fa86cb
YEJIN <ye_s_jin@naver.com>
2025-05-20 20:18:29 +0900

    outfit error screen

---
+++ app/src/main/java/com/jin/jjinweather/feature/outfitrecommend/ui/OutfitRecommendScreen.kt
@@ -0,0 +1,99 @@
+package com.jin.jjinweather.feature.outfitrecommend.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.jin.jjinweather.R
+import com.jin.jjinweather.feature.weather.ui.state.UiState
+import com.jin.jjinweather.ui.theme.ButtonColor
+import com.jin.jjinweather.ui.theme.JJinWeatherTheme
+
+@Composable
+fun OutfitRecommendScreen(imageUrlState: UiState<String>) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            when (imageUrlState) {
+                is UiState.Success -> {
+                    AsyncImage(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp),
+                        model = imageUrlState.data,
+                        contentDescription = ""
+                    )
+                }
+
+                else -> {
+                    OutfitError()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun OutfitError() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            painter = painterResource(R.drawable.img_outfit_error),
+            contentDescription = ""
+        )
+        Text(
+            "현재 온도에 맞는 옷을 찾지 못했습니다.",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(top = 20.dp)
+        )
+        //FIXME : 재요청?
+        Box(
+            modifier = Modifier
+                .clickable { }
+                .clip(RoundedCornerShape(8.dp))
+                .background(ButtonColor)
+                .padding(horizontal = 4.dp),
+        ) {
+            Text(
+                text = "재요청",
+                fontSize = 10.sp,
+                color = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ErrorPreview() {
+    JJinWeatherTheme {
+        OutfitError()
+    }
+}
