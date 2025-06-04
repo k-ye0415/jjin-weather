7a37bac7b6c5aae1e7040ed7e7d418b022ccb07b
YEJIN <ye_s_jin@naver.com>
2025-06-04 13:50:01 +0900

    places 초기화 전 조건 제거

--- app/src/main/java/com/jin/jjinweather/MyApp.kt
+++ app/src/main/java/com/jin/jjinweather/MyApp.kt
@@ -7,8 +7,6 @@ import java.util.Locale
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (!Places.isInitialized()) {
-            Places.initialize(applicationContext, BuildConfig.GOOGLE_PLACES_API_KEY, Locale.KOREA)
-        }
+        Places.initialize(applicationContext, BuildConfig.GOOGLE_PLACES_API_KEY, Locale.KOREA)
     }
 }
