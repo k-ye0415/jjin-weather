fd541ffa69f94cdd8be25509a3bfc1630d675fa9
YEJIN <ye_s_jin@naver.com>
2025-06-13 18:48:13 +0900

    weather data 요청 및 저장

--- app/src/main/java/com/jin/jjinweather/feature/location/domain/SaveDistrictAndRequestWeatherUseCase.kt
+++ app/src/main/java/com/jin/jjinweather/feature/location/domain/SaveDistrictAndRequestWeatherUseCase.kt
@@ -2,10 +2,15 @@ package com.jin.jjinweather.feature.location.domain
 
 import com.jin.jjinweather.feature.googleplaces.domain.model.District
 import com.jin.jjinweather.feature.location.LocationRepository
+import com.jin.jjinweather.feature.weather.domain.repository.WeatherRepository
 
-class SaveDistrictAndRequestWeatherUseCase(private val locationRepository: LocationRepository) {
+class SaveDistrictAndRequestWeatherUseCase(
+    private val locationRepository: LocationRepository,
+    private val weatherRepository: WeatherRepository
+) {
     suspend operator fun invoke(district: District) {
         locationRepository.insertGeoPoint(district.geoPoint)
         locationRepository.insertCityName(district.address)
+        weatherRepository.weatherAt(district.geoPoint.latitude, district.geoPoint.longitude)
     }
 }
