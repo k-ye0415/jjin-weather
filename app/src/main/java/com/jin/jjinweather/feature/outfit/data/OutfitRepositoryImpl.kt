78f5456a7ff633f7ec73d4bfbd539ef39be2617d
YEJIN <ye_s_jin@naver.com>
2025-06-23 23:45:08 +0900

    storage, firestore data 저장

--- app/src/main/java/com/jin/jjinweather/feature/outfit/data/OutfitRepositoryImpl.kt
+++ app/src/main/java/com/jin/jjinweather/feature/outfit/data/OutfitRepositoryImpl.kt
@@ -1,9 +1,16 @@
 package com.jin.jjinweather.feature.outfit.data
 
 import android.content.Context
+import android.net.Uri
 import android.util.Log
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.jin.jjinweather.feature.outfit.domain.OutfitRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -21,16 +28,20 @@ class OutfitRepositoryImpl(
     override suspend fun generateOutfitImageUrl(temperature: Int): Result<String> {
         return openAiDataSource.generateImagePrompt(temperature).fold(
             onSuccess = {
-                val imageUrl = dalleDataSource.requestOutfitImageGeneration(it)
-                downloadImageUrlToFile(imageUrl.getOrNull().orEmpty())
-                imageUrl
-//                Result.success("https://picsum.photos/1792/1024?random=1") // test
+//                val imageUrl = dalleDataSource.requestOutfitImageGeneration(it)
+//                val file = downloadImageUrlToFile(imageUrl.getOrNull().orEmpty())
+                val file = File(context.cacheDir, "0add5a9b-6090-47b1-a209-e0991ed1b348.png")
+                val downloadUrl = uploadStorage(file)
+//                val downloadUrl = "https://firebasestorage.googleapis.com/v0/b/jjin-weather.firebasestorage.app/o/images%2F0add5a9b-6090-47b1-a209-e0991ed1b348.png?alt=media&token=9e811776-ef30-4b5f-a501-4b734f761c94"
+                uploadFireStore(temperature, downloadUrl)
+//                imageUrl
+                Result.success("https://picsum.photos/1792/1024?random=1") // test
             },
             onFailure = { Result.failure(it) }
         )
     }
 
-    private suspend fun downloadImageUrlToFile(imageUrl: String) {
+    private suspend fun downloadImageUrlToFile(imageUrl: String): File =
         withContext(Dispatchers.IO) {
             val client = OkHttpClient()
             val request = Request.Builder()
@@ -64,8 +75,35 @@ class OutfitRepositoryImpl(
             file.outputStream().use { output ->
                 inputStream?.copyTo(output)
             }
+            return@withContext file
+        }
+
+    private suspend fun uploadStorage(file: File): String =
+        withContext(Dispatchers.IO) {
+            val storage = Firebase.storage
+            val fileName = file.name
+            val imageRef = storage.reference.child("images/$fileName")
+
+            imageRef.putFile(Uri.fromFile(file)).await()
+
+            return@withContext imageRef.downloadUrl.await().toString()
+        }
+
+
+    private suspend fun uploadFireStore(temperature: Int, imageUrl: String) {
+        withContext(Dispatchers.IO) {
+            // FIXME DI
+            val firestore = Firebase.firestore("weather")
+            val key = "$temperature"
+            val data = mapOf(key to FieldValue.arrayUnion(imageUrl))
+
+            val docRef = firestore.collection("outfit").document("images")
+            docRef.set(data, SetOptions.merge())
+
+//            file.delete()
         }
     }
+
 
     private companion object {
         val TAG = "OutfitRepository"
